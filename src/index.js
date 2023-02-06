bba8c4ec5d4b339da8ec2063fa61acd23f6575e3
YevheniiaVier <gotashk1983@gmail.com>
2023-02-06 10:39:48 +0200

    add first redux version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from 'components/App/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
