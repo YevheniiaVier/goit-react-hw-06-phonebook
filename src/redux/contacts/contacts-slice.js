a799dfaf6dfa073f1b1d3801792049eb4c059702
YevheniiaVier <gotashk1983@gmail.com>
2023-02-11 11:01:34 +0200

    corrects commets

--- src/redux/contacts/contacts-slice.js
+++ src/redux/contacts/contacts-slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import { store } from 'redux/store';
 import shortid from 'shortid';
 
 import initialContacts from '../../contacts.json';
@@ -25,18 +24,9 @@ const contactsSlice = createSlice({
       store.filter(item => item.id !== payload),
 
     editContact: (store, { payload }) => {
-      // const index = store.findIndex(({ id }) => id === payload.id);
-
-      // return store.map((item, i) => (i === index ? payload : item));
       return store.map(contact =>
         contact.id === payload.id ? payload : contact
       );
-      // console.log('i', index);
-      // console.log('p', payload);
-      // console.log('st i', store[index]);
-
-      // // const index = store.indexOf(item);
-      // store[index] = payload;
     },
   },
 });
